f781de07d0b34abf45a6ea7f1c1218ea1fcf081e
Alihan_8 <alihanmi001@e.ntu.edu.sg>
2026-09-05 23:34:47 +0800

    Implement initial order matching prototype

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,35 @@
 #include <bits/stdc++.h>
 
+#include "order_book.hpp"
+
 using namespace std;
 
+int cntTime = 0;
+
+int getTime(){ return cntTime++; }
+
 signed main(){
-    cout << "FUck u\n";
+    int t; cin >> t;
+
+    OrderBook exchange;
+
+    while ( t-- ){
+        string op; cin >> op;
+
+        if ( op == "add" ){
+            string type; int id, price, quantity; cin >> type >> id >> price >> quantity;
+
+            exchange.AddOrder(Order(id, getTime(), price, quantity, type == "buy")); 
+        } else if ( op == "rmv" ){
+            int id; cin >> id;
+
+            exchange.RemoveOrder(id);
+        } else{
+            int id; cin >> id;
+
+            exchange.GetOrder(id).print();
+        }
+    }
+
+    exchange.PrintLog();
 }
